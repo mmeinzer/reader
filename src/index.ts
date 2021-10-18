371e0699441ad47851ff9ea22ee710973d41c3fe
Matt Meinzer <meinzer.matthew@gmail.com>
2021-10-18 17:20:28 -0500

    Add error handling to main

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import ON_DEATH from "death";
+import { Database } from "sqlite";
 import openDb from "./db";
 import { newRepo } from "./repo";
 import { startServer } from "./server";
 
 (async function main() {
-  const db = await openDb(process.env.DB_FILENAME ?? "./db");
+  let db: Database | null;
+  try {
+    db = await openDb(process.env.DB_FILENAME ?? "./db");
+  } catch (err) {
+    console.error(err)
+    db = null
+  }
+
   if (!db) {
-    console.log("error intializing db");
+    console.error("error intializing db");
     process.exit(1);
   }
 
@@ -16,9 +24,16 @@ import { startServer } from "./server";
 
   ON_DEATH(async (sig) => {
     console.log(`got signal ${sig}`);
+
     server.close(() => {
       console.log("HTTP server closed");
     });
+
+    if (!db) {
+      console.warn("db was null in server close function")
+      process.exit(0)
+    }
+
     await db.close();
     console.log("database closed");
     process.exit(0);
